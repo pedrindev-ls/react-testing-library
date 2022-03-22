32bde18759790e2f312ce713646b2099a0f2fd3b
Pedrin <pedro515vieira@gmail.com>
2022-03-22 20:21:24 -0300

    :heavy_check_mark: Requisito 4 finalizado :construction: e requisito 3 iniciad

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('Testa se no Component Favorite Pokemon', () => {
+  test('Aparece "No favorite pokemon found" caso não haja pokemon favoritado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const noFavs = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavs).toBeInTheDocument();
+  });
+
+  test('Aparece todos os pokemons favoritados', () => {
+
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('Testa se Ao digitar um url errado', () => {
+  test('Aparece o texto "Page requested not found 😭"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aleatorio');
+    const text = screen
+      .getByRole('heading', { name: /Page requested not found/i }, { level: 2 });
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Aparece uma imagem com a url "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aleatorio');
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found')
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
